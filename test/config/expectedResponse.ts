aca61474c0bb8f5333c7c2bc876e6e2cb1274899
Jorge Hernandez <jorge.hernandez@dreamcodesoft.com>
2021-04-28 22:54:15 -0500

    unit test 2

--- test/config/expectedResponse.ts
+++ test/config/expectedResponse.ts
@@ -1,3 +1,7 @@
 export const EXPECTED_RESPONSE = {
-    EXPECTED_RESPONSE_1:{"data": 1000, "message": "SUCCESS", "statusCode": 200}
-}
+  EXPECTED_RESPONSE_1: { data: 1000, message: "SUCCESS", statusCode: 200 },
+};
+
+export const BAD_RESPONSE = {
+  BAD_RESPONSE_1: { data: null, message: "ERROR", statusCode: 500 },
+};
--- test/controller/processFileController.test.ts
+++ test/controller/processFileController.test.ts
@@ -1,15 +1,15 @@
 import { Database } from "../../src/db/mongoDBSettings";
 import csvtojson from "csvtojson";
 import { toJsonMock, request } from "../config/commonMocks";
-import {EXPECTED_RESPONSE} from '../config/expectedResponse'
+import {EXPECTED_RESPONSE,BAD_RESPONSE} from '../config/expectedResponse'
 import { ProcessFileController } from "../../src/controllers/processFileController";
 
 const toJson = csvtojson();
 const mongoDb = new Database();
 const processFileController = new ProcessFileController();
 
-describe("test", async () => {
-  it("test", async () => {
+describe("ProcessFileController", async () => {
+  it("Success response", async () => {
     const jsonMock: any = toJsonMock;
     const toJsonFrom: any = jest
       .spyOn(toJson, "fromFile")
@@ -21,5 +21,14 @@ describe("test", async () => {
     });
     expect(response).toEqual(EXPECTED_RESPONSE.EXPECTED_RESPONSE_1);
     toJsonFrom.mockRestore();
+  });
+
+  it("Bad response", async () => {
+    const response: any = await processFileController.processFile(request, {
+      json: () => {
+        return { statusCode: 500, message: "ERROR", data: null };
+      },
+    });
+    expect(response).toEqual(BAD_RESPONSE.BAD_RESPONSE_1);
   });
 });
